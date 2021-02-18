baf66cf651f293938a5edfb5c7111caa5cd7772d
sapru-nitin@outlook.com <Apple@0620>
2021-02-18 17:25:41 +0530

    Fixed a data model.

---
+++ Meetings/Models/Attendee.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Microsoft.Teams.Api.Meetings.Models
+{
+    /// <summary>
+    ///     Represents the attendee of the meeting who are not invited as part of the actual
+    ///     meeting invite.
+    /// </summary>
+    public class Attendee
+    {
+        /// <summary>
+        ///     Gets or sets the identity of the user.
+        /// </summary>
+        public Identity Identity { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Upn of the user.
+        /// </summary>
+        public string Upn { get; set; }
+    }
+}
